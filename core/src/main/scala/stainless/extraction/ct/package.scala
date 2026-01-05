507def0102ef4dda55be87b3eaef1a65f4f489ca
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2026-01-05 01:15:14 +0000

    Implement lockstep instrumentation

--- core/src/main/scala/stainless/extraction/ct/package.scala
+++ core/src/main/scala/stainless/extraction/ct/package.scala
@@ -15,12 +15,82 @@ class Instrumentation(override val s: xlang.trees.type, override val t: xlang.tr
     override protected type TransformerContext = Unit
     override protected def getContext(symbols: s.Symbols): Unit = {}
 
-    override protected def extractFunction(context: TransformerContext, fd: s.FunDef): (t.FunDef, Unit) = {
-        val newfd = fd.fullBody match
-            case s.Let(a1, a2, body) =>
-                val newAssert = s.Assert(s.Equals(s.IntegerLiteral(1), s.IntegerLiteral(1)), None, body)
-                fd.copy(fullBody = s.Let(a1, a2, newAssert))
-            case _ => fd
-        (newfd, ())
+    private def freshShadowId(id: Identifier): Identifier =
+        FreshIdentifier(id.name + "_shadow")
+
+    private def shadowExpression(expression: s.Expr, idToShadowId: Map[Identifier, Identifier]): s.Expr =
+
+        def updateToShadowVariableIfApplicable(source: s.exprOps.Source):  Option[s.exprOps.Source] =
+            source match
+                case s.Variable(id, tpe, flags) if idToShadowId.contains(id) =>
+                    Some(s.Variable(idToShadowId(id), tpe, flags))
+                case _ =>
+                    None
+
+        s.exprOps.preMap(updateToShadowVariableIfApplicable)(expression)
+
+    private def lockstepExpression(expression: s.Expr, idToShadowId: Map[Identifier, Identifier]): s.Expr =
+        expression match
+            case s.Let(valDef, value, body) =>
+                val shadowValDefId = freshShadowId(valDef.id)
+
+                val lockstepValue = lockstepExpression(value, idToShadowId)
+                val lockstepBody = lockstepExpression(body, idToShadowId + (valDef.id -> shadowValDefId))
+
+                val newLet = s.Let(valDef, lockstepValue, lockstepBody)
+
+                val shadowValDef = s.ValDef(shadowValDefId, valDef.tpe, valDef.flags)
+                val shadowValue = shadowExpression(value, idToShadowId)
+
+                s.Let(
+                    shadowValDef,
+                    shadowValue,
+                    newLet
+                )
+
+            case s.IfExpr(condition, thenBranch, elseBranch) =>
+                val lockstepCondition = lockstepExpression(condition, idToShadowId)
+                val lockstepThenBranch = lockstepExpression(thenBranch, idToShadowId)
+                val lockstepElseBranch = lockstepExpression(elseBranch, idToShadowId)
+
+                val newIfExpr = s.IfExpr(lockstepCondition, lockstepThenBranch, lockstepElseBranch)
+
+                val shadowCondition = shadowExpression(condition, idToShadowId)
+                val assertPredicate = s.Equals(condition, shadowCondition)
+
+                s.Assert(
+                    assertPredicate,
+                    None,
+                    newIfExpr
+                )
+            case _ =>
+                expression
+
+    override protected def extractFunction(context: TransformerContext, function: s.FunDef): (t.FunDef, Unit) = {
+        val secretArgumentId = function.params.find(_.id.name == "secret").get.id
+        val publicArgumentId = function.params.find(_.id.name == "public").get.id
+
+        val idToShadowId = Map(
+            secretArgumentId -> freshShadowId(secretArgumentId),
+            publicArgumentId -> freshShadowId(publicArgumentId)
+        )
+
+        val shadowSecretArgumentValDef = s.ValDef(idToShadowId(secretArgumentId), s.IntegerType())
+        val shadowPublicArgumentValDef = s.ValDef(idToShadowId(publicArgumentId), s.IntegerType())
+
+        val lockstepBody = lockstepExpression(function.fullBody, idToShadowId)
+
+        val lockstepBodyWithShadowPublic = s.Let(
+            shadowPublicArgumentValDef,
+            s.Variable(publicArgumentId, s.IntegerType(), Seq.empty),
+            lockstepBody
+        )
+
+        val instrumentedFunction = function.copy(
+            params = function.params :+ shadowSecretArgumentValDef,
+            fullBody = lockstepBodyWithShadowPublic
+        )
+
+        (instrumentedFunction, ())
     }
 }
