18479a90f64c11f44cd4b277bfffbeca3b3ef35d
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2026-01-06 00:35:17 +0000

    Implement product return type

--- core/src/main/scala/stainless/extraction/ct/package.scala
+++ core/src/main/scala/stainless/extraction/ct/package.scala
@@ -15,6 +15,9 @@ class Instrumentation(override val s: xlang.trees.type, override val t: xlang.tr
     override protected type TransformerContext = Unit
     override protected def getContext(symbols: s.Symbols): Unit = {}
 
+    private def productType(originalType: s.Type): s.Type =
+        t.TupleType(Seq(originalType, originalType))
+
 
     private def lockstepExpression(expression: s.Expr, idToShadowId: Map[Identifier, Identifier]): s.Expr =
         expression
@@ -23,8 +26,8 @@ class Instrumentation(override val s: xlang.trees.type, override val t: xlang.tr
         val originalSecretValDef = function.params.find(_.id.name == "secret").get
         val originalPublicValDef = function.params.find(_.id.name == "public").get
 
-        val productSecretValDef = t.ValDef.fresh("secret", t.TupleType(Seq(originalSecretValDef.tpe, originalSecretValDef.tpe)))
-        val productPublicValDef = t.ValDef.fresh("public", t.TupleType(Seq(originalPublicValDef.tpe, originalPublicValDef.tpe)))
+        val productSecretValDef = t.ValDef.fresh("secret", productType(originalSecretValDef.tpe))
+        val productPublicValDef = t.ValDef.fresh("public", productType(originalPublicValDef.tpe))
 
         val idToProductId = Map(
             originalSecretValDef.id -> productSecretValDef.id,
@@ -42,7 +45,8 @@ class Instrumentation(override val s: xlang.trees.type, override val t: xlang.tr
 
         val instrumentedFunction = function.copy(
             params = Seq(productSecretValDef, originalPublicValDef),
-            fullBody = lockstepBodyWithProductPublicInitialization
+            fullBody = lockstepBodyWithProductPublicInitialization,
+            returnType = productType(function.returnType)
         )
 
         instrumentedFunction
