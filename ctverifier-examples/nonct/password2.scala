33cc534070f8abadd42a9a184146351482c6fe80
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2026-01-06 10:06:04 +0000

    Add nonct password example short circuit

---
+++ ctverifier-examples/nonct/password2.scala
@@ -0,0 +1,17 @@
+import stainless.annotation._
+import stainless.collection._
+
+@ctverify
+def checkPassword(@secret correctPassword: List[Char], attemptPassword: List[Char]): Boolean =
+  @public val passwordSize = correctPassword.size
+
+  (correctPassword, attemptPassword) match
+    case (Cons(correctChar, correctRest), Cons(attemptChar, attemptRest)) =>
+      (correctChar == attemptChar) && checkPassword(correctRest, attemptRest)
+
+    case (Nil(), Nil()) =>
+      true
+
+    case _ =>
+      false
+
