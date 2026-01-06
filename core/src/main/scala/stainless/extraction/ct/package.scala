ecaf53bff4c88206e0a5aea715f1997b3f49e8c9
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2026-01-06 00:27:19 +0000

    New lockstep implementation : arguments

--- core/src/main/scala/stainless/extraction/ct/package.scala
+++ core/src/main/scala/stainless/extraction/ct/package.scala
@@ -15,112 +15,34 @@ class Instrumentation(override val s: xlang.trees.type, override val t: xlang.tr
     override protected type TransformerContext = Unit
     override protected def getContext(symbols: s.Symbols): Unit = {}
 
-    private def freshShadowId(id: Identifier): Identifier =
-        FreshIdentifier(id.name + "_shadow")
-
-    private def shadowExpression(expression: s.Expr, idToShadowId: Map[Identifier, Identifier]): s.Expr =
-
-        def updateToShadowVariableIfApplicable(source: s.exprOps.Source):  Option[s.exprOps.Source] =
-            source match
-                case s.Variable(id, tpe, flags) if idToShadowId.contains(id) =>
-                    Some(s.Variable(idToShadowId(id), tpe, flags))
-                case _ =>
-                    None
-
-        s.exprOps.preMap(updateToShadowVariableIfApplicable)(expression)
 
     private def lockstepExpression(expression: s.Expr, idToShadowId: Map[Identifier, Identifier]): s.Expr =
-        expression match
-            case s.Let(valDef, value, body) =>
-                val shadowValDefId = freshShadowId(valDef.id)
-
-                val lockstepValue = lockstepExpression(value, idToShadowId)
-                val lockstepBody = lockstepExpression(body, idToShadowId + (valDef.id -> shadowValDefId))
-
-                val newLet = s.Let(valDef, lockstepValue, lockstepBody)
-
-                val shadowValDef = s.ValDef(shadowValDefId, valDef.tpe, valDef.flags)
-                val shadowValue = shadowExpression(value, idToShadowId)
-
-                s.Let(
-                    shadowValDef,
-                    shadowValue,
-                    newLet
-                )
-
-            case s.IfExpr(condition, thenBranch, elseBranch) =>
-                val lockstepCondition = lockstepExpression(condition, idToShadowId)
-                val lockstepThenBranch = lockstepExpression(thenBranch, idToShadowId)
-                val lockstepElseBranch = lockstepExpression(elseBranch, idToShadowId)
-
-                val newIfExpr = s.IfExpr(lockstepCondition, lockstepThenBranch, lockstepElseBranch)
-
-                val shadowCondition = shadowExpression(condition, idToShadowId)
-                val assertPredicate = s.Equals(condition, shadowCondition)
-
-                s.Assert(
-                    assertPredicate,
-                    None,
-                    newIfExpr
-                )
-
-            case s.MatchExpr(scrutinee, cases) =>
-
-                val lockstepMatchCases = cases.map(matchCase =>
-                    matchCase.copy(
-                        rhs = lockstepExpression(matchCase.rhs, idToShadowId)
-                    )
-                )
-
-                val newMatchExpr = s.MatchExpr(scrutinee, lockstepMatchCases)
-
-                def buildExecutionPathCase(originalCase: s.MatchCase, pathIndex: Int): s.MatchCase =
-                    originalCase.copy(rhs=s.IntegerLiteral(pathIndex))
-                val executionPathMatchCases = cases.zipWithIndex.map(buildExecutionPathCase)
-
-                val executionPathMatchExpression = s.MatchExpr(
-                    scrutinee,
-                    executionPathMatchCases
-                )
-                val shadowExecutionPathMatchExpression = s.MatchExpr(
-                    shadowExpression(scrutinee, idToShadowId),
-                    executionPathMatchCases
-                )
-
-                val assertPredicate = s.Equals(executionPathMatchExpression, shadowExecutionPathMatchExpression)
-
-                s.Assert(
-                    assertPredicate,
-                    None,
-                    newMatchExpr
-                )
-
-            case _ =>
-                expression
+        expression
 
     private def instrumentFunction(function: s.FunDef): s.FunDef =
-        val secretArgumentId = function.params.find(_.id.name == "secret").get.id
-        val publicArgumentId = function.params.find(_.id.name == "public").get.id
+        val originalSecretValDef = function.params.find(_.id.name == "secret").get
+        val originalPublicValDef = function.params.find(_.id.name == "public").get
 
-        val idToShadowId = Map(
-            secretArgumentId -> freshShadowId(secretArgumentId),
-            publicArgumentId -> freshShadowId(publicArgumentId)
+        val productSecretValDef = t.ValDef.fresh("secret", t.TupleType(Seq(originalSecretValDef.tpe, originalSecretValDef.tpe)))
+        val productPublicValDef = t.ValDef.fresh("public", t.TupleType(Seq(originalPublicValDef.tpe, originalPublicValDef.tpe)))
+
+        val idToProductId = Map(
+            originalSecretValDef.id -> productSecretValDef.id,
+            originalPublicValDef.id -> productPublicValDef.id
         )
 
-        val shadowSecretArgumentValDef = s.ValDef(idToShadowId(secretArgumentId), s.IntegerType())
-        val shadowPublicArgumentValDef = s.ValDef(idToShadowId(publicArgumentId), s.IntegerType())
+        val lockstepBody = lockstepExpression(function.fullBody, idToProductId)
 
-        val lockstepBody = lockstepExpression(function.fullBody, idToShadowId)
-
-        val lockstepBodyWithShadowPublic = s.Let(
-            shadowPublicArgumentValDef,
-            s.Variable(publicArgumentId, s.IntegerType(), Seq.empty),
+        val originalPublicVariable = s.Variable(originalPublicValDef.id, originalPublicValDef.tpe, Seq.empty)
+        val lockstepBodyWithProductPublicInitialization = s.Let(
+            productPublicValDef,
+            s.Tuple(Seq(originalPublicVariable, originalPublicVariable)),
             lockstepBody
         )
 
         val instrumentedFunction = function.copy(
-            params = function.params :+ shadowSecretArgumentValDef,
-            fullBody = lockstepBodyWithShadowPublic
+            params = Seq(productSecretValDef, originalPublicValDef),
+            fullBody = lockstepBodyWithProductPublicInitialization
         )
 
         instrumentedFunction
